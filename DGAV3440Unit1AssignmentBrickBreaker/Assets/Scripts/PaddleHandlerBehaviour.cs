2b6f38a737b4aa1e6609c55d9693ef1869d79d40
Valeska <valeskabagaric@hotmail.com>
2023-09-02 01:09:20 -0600

    Added Input system, Imported Models form maya, Added Script Library

---
+++ DGAV3440Unit1AssignmentBrickBreaker/Assets/Scripts/PaddleHandlerBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PaddleHandlerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        
+        Debug.Log(touchPosition);
+    }
+}
