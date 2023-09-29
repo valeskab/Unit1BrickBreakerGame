e7b76a89fa3b1d50fa953d93fee2184dde2cb0b5
Valeska <valeskabagaric@hotmail.com>
2023-09-29 17:18:35 -0600

    Fixing game, touch position

--- DGAV3440Unit1AssignmentBrickBreaker/Assets/Scripts/BallController.cs
+++ DGAV3440Unit1AssignmentBrickBreaker/Assets/Scripts/BallController.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera mainCamera;
+
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            Debug.Log(touchPosition);
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+            Debug.Log(worldPosition);
+        }
     }
 }
